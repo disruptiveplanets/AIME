b71372c24e0e74e19c3fcb69e7fe06eea45921d8
JackDinsmore <goodsirmango@gmail.com>
2021-07-21 11:45:36 -0500

    Prepared for order 2 fit

--- data/display.py
+++ data/display.py
@@ -16,29 +16,29 @@ except AttributeError:
     print("Could not find the file {}.".format(filename))
     sys.exit()
 
-try:
+#try:
 
-    tau = reader.get_autocorr_time()
-    burnin = int(2 * np.max(tau))
-    thin = int(0.5 * np.min(tau))
-    samples = reader.get_chain(discard=burnin, flat=True, thin=thin)
-    log_prob_samples = reader.get_log_prob(discard=burnin, flat=True, thin=thin)
-    log_prior_samples = reader.get_blobs(discard=burnin, flat=True, thin=thin)
+tau = reader.get_autocorr_time()
+burnin = int(2 * np.max(tau))
+thin = int(0.5 * np.min(tau))
+samples = reader.get_chain(discard=burnin, flat=True, thin=thin)
+log_prob_samples = reader.get_log_prob(discard=burnin, flat=True, thin=thin)
+log_prior_samples = reader.get_blobs(discard=burnin, flat=True, thin=thin)
 
-    print("burn-in: {0}".format(burnin))
-    print("thin: {0}".format(thin))
-    print("flat chain shape: {0}".format(samples.shape))
-    print("flat log prob shape: {0}".format(log_prob_samples.shape))
-    print("flat log prior shape: {0}".format(log_prior_samples.shape))
+print("burn-in: {0}".format(burnin))
+print("thin: {0}".format(thin))
+print("flat chain shape: {0}".format(samples.shape))
+print("flat log prob shape: {0}".format(log_prob_samples.shape))
+print("flat log prior shape: {0}".format(log_prior_samples.shape))
 
-except:
+'''except AutocorrError:
     print("Could not find autocorrelation time because the chain is too short.")
     samples = reader.get_chain(flat=True)
     log_prob_samples = reader.get_log_prob(flat=True)
     log_prior_samples = reader.get_blobs(flat=True)
     print(log_prob_samples)
     print(log_prior_samples)
-    sys.exit()
+    sys.exit()'''
 
 all_samples = np.concatenate(
     (samples, log_prob_samples[:, None], log_prior_samples[:, None]), axis=1
