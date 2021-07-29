77bd82ea36caff4a470c0e54b5e3091c895aab27
JackDinsmore <goodsirmango@gmail.com>
2021-07-29 18:50:32 -0500

    First progress report

--- code/fit_resolved/call.py
+++ code/fit_resolved/call.py
@@ -1,16 +1,22 @@
 import asteroids, time, sys
+import matplotlib.pyplot as plt
+from random_vector import randomize
+import numpy as np
 
-CADENCE = 3600.0
+CADENCE = 600.0
 
-jlms = [5.972e24, 0, 0, 0]
-klms = [
-    1e6, 1e5, -5e5,
-    0, 0, 0, 0, 0, 0, 0 #m3
-    ]
-spin = [0.00012, 0.00012, 0.00012]
-initial_roll = 0
-impact_parameter = 31850000
+EARTH_RADIUS = 6370000
+EARTH_MASS =5.972e24
+
+spin = [0.00012, 0.00022, 0.00032]
+impact_parameter = 5 * EARTH_RADIUS
 speed = 4000
+jlms = [EARTH_MASS, 0, 6e22, 7e22]
+klms = [
+    1.2e6, 1.1e5, -4.9e5,
+]
+SIGMA = 0.2 * np.sqrt(spin[0]**2 + spin[1]**2 + spin[2]**2)
+initial_roll = 0
 
 start = time.time()
 try:
@@ -28,3 +34,17 @@ for i, dat in enumerate(resolved_data):
     if i %3 == 2:
         f.write('\n')
 f.close()
+
+
+y, yerr = randomize(resolved_data, SIGMA)
+
+plt.figure(figsize=(12, 4))
+x_display = np.arange(len(y) / 3)
+plt.errorbar(x_display, y[::3], yerr=yerr[::3], label = 'x', fmt='.')
+plt.errorbar(x_display, y[1::3], yerr=yerr[1::3], label = 'y', fmt='.')
+plt.errorbar(x_display, y[2::3], yerr=yerr[2::3], label = 'z', fmt='.')
+plt.xlabel("Time (Cadences)")
+plt.ylabel("Spin (rad/s)")
+plt.legend()
+plt.savefig("resolved_data.png")
+plt.show()
