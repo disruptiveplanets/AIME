d99bd30fef9417f88605dee358020197e5143139
JackDinsmore <goodsirmango@gmail.com>
2021-08-05 10:17:16 -0400

    Staged for reload

--- data/converge/readme.md
+++ data/converge/readme.md
@@ -5,3 +5,7 @@ My goal is to get the fit to converge.
 ## August 4
 
 I want to try setting a large cloud, so I have run 0. Then I implement Julien's advice of setting three parameters to the true values and varying one (runs 1.0-1.3)
+
+## August 5
+
+The fits look good! They're much closer to converging, but I need to run them again.
--- data/display.py
+++ data/display.py
@@ -1,4 +1,4 @@
-import sys, corner, emcee, os
+import sys, corner, emcee, os, asteroids
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,6 +10,7 @@ class Display:
         self.reader = emcee.backends.HDFBackend(self.bare_name+".h5", read_only=True)
         self.samples = None
         self.theta_true = None
+        self.true_results = None
         try:
             self.chain_length, self.nwalkers, self.ndim = self.reader.get_chain().shape
         except AttributeError:
@@ -25,16 +26,15 @@ class Display:
             self.thin = int(0.5 * np.min(tau))
         except:
             print("Could not find autocorrelation time because the chain is too short.")
+            self.thin = 1
             self.get_samples_burnin(0)
             self.burnin = 0
-            self.thin = 1
             self.show_params()
-            self.show_redchi()
             plt.show()
             self.burnin = -1
             while self.burnin < 0:
                 try:
-                    self.burnin = int(input("What is the burnin?"))
+                    self.burnin = int(input("What is the burnin? "))
                 except:
                     pass
 
@@ -46,15 +46,15 @@ class Display:
         print("flat log prob shape: {0}".format(self.log_prob_samples.shape))
 
     def get_samples_burnin(self, burnin):
-        self.samples = self.reader.get_chain(discard=burnin, flat=True, thin=self.thin)
-        self.log_prob_samples = self.reader.get_log_prob(discard=burnin, flat=True, thin=self.thin)
-        self.log_prior_samples = self.reader.get_blobs(discard=burnin, flat=True, thin=self.thin)
+        self.samples = self.reader.get_chain(discard=burnin, thin=self.thin)
+        self.log_prob_samples = self.reader.get_log_prob(discard=burnin, thin=self.thin)
+        self.log_prior_samples = self.reader.get_blobs(discard=burnin, thin=self.thin)
 
 
     def show_params(self):
         self.get_samples()
         fig, axes = plt.subplots(self.ndim, figsize=(10, 7), sharex=True)
-        samples = self.sampler.get_chain()
+        samples = self.reader.get_chain()
         for i in range(self.ndim):
             ax = axes[i]
             ax.plot(self.samples[:, :, i], "k", alpha=0.3)
@@ -65,20 +65,28 @@ class Display:
         plt.savefig(self.bare_name+"-params.png")
 
     def show_redchi(self):
+        self.get_params()
         self.get_samples()
+        if self.true_results is None:
+            self.true_results = self.run(self.theta_true)
         fig = plt.figure(figsize=(10, 7),)
+        redchiminmean = 0
         for i in range(self.log_prob_samples.shape[1]):
-            plt.plot(-self.log_prob_samples[:,i] / dof, c='k', alpha=0.25)
+            redchi = -self.log_prob_samples[:,i] / len(self.true_results)
+            redchiminmean += np.min(redchi) / self.log_prob_samples.shape[1]
+            plt.plot(redchi, c='k', alpha=0.25)
         plt.ylabel("Reduced chi squared")
         plt.xlabel("Sample")
-        plt.ylim(0, 1e5)
+        plt.ylim(0, max(10, 2 * redchiminmean))
         plt.savefig(self.bare_name+"-redchi.png")
 
     def show_corner(self):
         self.get_samples()
         self.get_params()
+        flat_samples = self.samples.reshape(
+            (self.samples.shape[0] * self.samples.shape[1], self.samples.shape[2]))
         fig = corner.corner(
-            self.samples, labels=self.theta_labels, truths=self.theta_true
+            flat_samples, labels=self.theta_labels, truths=self.theta_true
         );
         plt.savefig(self.bare_name+"-corner.png")
 
@@ -109,33 +117,38 @@ class Display:
     def get_results(self):
         self.get_samples()
         res = []
+        flat_samples = self.samples.reshape(
+            (self.samples.shape[0] * self.samples.shape[1], self.samples.shape[2]))
         for i in range(self.ndim):
-            mcmc = np.percentile(self.samples[:, i], [16, 50, 84])
+            mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
             q = np.diff(mcmc)
             res.append([mcmc[1], q[0], q[1]])
         return res
 
     def run(self, theta):
-        resolved_data = asteroids.simulate(cadence, jlms, theta[1:],
-            spin[0], spin[1], spin[2], theta[0], impact_parameter, speed)
+        self.get_params()
+        resolved_data = asteroids.simulate(self.cadence, self.jlms, theta[1:],
+            self.spin[0], self.spin[1], self.spin[2], theta[0],
+            self.impact_parameter, self.speed)
         return np.asarray(resolved_data)
 
     def show_compare(self):
         self.get_params()
         theta_results = [f[0] for f in self.get_results()]
-        true_res = self.run(self.theta_true)
+        if self.true_results is None:
+            self.true_results = self.run(self.theta_true)
         mean_res = self.run(theta_results)
 
         plt.figure(figsize=(12, 4))
 
-        x_display = np.arange(len(true_res) / 3)
-        plt.plot(x_display, true_res[::3], label = 'true x', alpha=0.5, color='lightsalmon')
-        plt.plot(x_display, true_res[1::3], label = 'true y', alpha=0.5, color='lightskyblue')
-        plt.plot(x_display, true_res[2::3], label = 'true z', alpha=0.5, color='moccasin')
+        x_display = np.arange(len(self.true_results) / 3)
+        plt.plot(x_display, self.true_results[::3], label = 'true x', alpha=0.5, color='C0')
+        plt.plot(x_display, self.true_results[1::3], label = 'true y', alpha=0.5, color='C1')
+        plt.plot(x_display, self.true_results[2::3], label = 'true z', alpha=0.5, color='C2')
 
-        plt.plot(x_display, mean_res[::3], label = 'mean x', alpha=0.5, linestyle='dotted', color='brown')
-        plt.plot(x_display, mean_res[1::3], label = 'mean y', alpha=0.5, linestyle='dotted', color='steelblue')
-        plt.plot(x_display, mean_res[2::3], label = 'mean z', alpha=0.5, linestyle='dotted', color='darkgoldenrod')
+        plt.plot(x_display, mean_res[::3], label = 'mean x', alpha=0.5, linestyle='dotted', color='C0')
+        plt.plot(x_display, mean_res[1::3], label = 'mean y', alpha=0.5, linestyle='dotted', color='C1')
+        plt.plot(x_display, mean_res[2::3], label = 'mean z', alpha=0.5, linestyle='dotted', color='C2')
         plt.xlabel("Time (Cadences)")
         plt.ylabel("Spin (rad/s)")
         plt.legend()
