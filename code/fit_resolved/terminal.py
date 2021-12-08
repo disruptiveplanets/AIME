d2ee67d5e6b9be010b16e01db2119b2d3a0e56ed
jack-dinsmore <goodsirmango@gmail.com>
2021-12-07 21:36:25 -0500

    Terminal program

--- code/fit_resolved/terminal.py
+++ code/fit_resolved/terminal.py
@@ -1,8 +1,17 @@
+TEST = False
+
 import numpy as np
 import matplotlib.pyplot as plt
-import emcee, time, sys, os
+import emcee, time, sys
+if not TEST:
+    import asteroids_0_2, asteroids_0_3, asteroids_2_2, asteroids_2_3, asteroids_3_2, asteroids_3_3
+if TEST:
+    import test_0_2 as asteroids_0_2
+    import test_0_2 as asteroids_0_3
+    import test_0_2 as asteroids_2_2
+    import test_0_2 as asteroids_2_3
 from multiprocessing import Pool
-from random_vector import *
+import random_vector, random
 from scipy import optimize, linalg
 from mpmath import mp
 import plotille
@@ -10,19 +19,16 @@ import display, collect
 
 import numdifftools as nd
 
+
+GM = 3.986004418e14
 EARTH_RADIUS = 6_370_000
 
-def process(output_name):
-    f = open("../../staged/" + output_name+".txt", 'r')
-    ASTEROIDS_MAX_J, ASTEROIDS_MAX_K = f.readline().split(', ')
-    ASTEROIDS_MAX_J = int(ASTEROIDS_MAX_J)
-    ASTEROIDS_MAX_K = int(ASTEROIDS_MAX_K)
+def terminal(output_name):
 
-    NUM_FITS = f.readline().split(', ')
-    NUM_FITS = [int(i) for i in NUM_FITS]
+    f = open("../../staged/" + output_name+".txt", 'r')
 
     cadence = int(f.readline())
-    impact_parameter = EARTH_RADIUS * int(f.readline())
+    perigee = EARTH_RADIUS * float(f.readline())
     radius = float(f.readline())
     speed = float(f.readline())
     spin = [float(x) for x in f.readline().split(',')]
@@ -31,20 +37,18 @@ def process(output_name):
     theta_high = np.asarray([float(x) for x in f.readline().split(',')])
     theta_low = np.asarray([float(x) for x in f.readline().split(',')])
 
-    sigma = float(f.readline()) * np.sqrt(spin[0]**2 + spin[1]**2 + spin[2]**2)
+    sigma = float(f.readline())
     while output_name[-1] == '\n':
         output_name = output_name[:-1]
     f.close()
-    assert(len(theta_true) == len(theta_high) == len(theta_low))
-    assert(len(theta_true) == (ASTEROIDS_MAX_K + 1)**2 - 6)
-    assert(len(jlms) == (ASTEROIDS_MAX_J + 1)**2)
-    assert(np.all(theta_high > theta_low))
 
-    NUM_FITS = NUM_FITS[:ASTEROIDS_MAX_K-1]
+    N_DIM = len(theta_true)
 
     ####################################################################
     # Process data
     ####################################################################
+    print()
+
     i = 0
     while True:
         print("Showing i = {}".format(i))
@@ -69,13 +73,8 @@ def process(output_name):
     # Save samples
     ####################################################################
 
-    index = 0
-    while True:
-        try:
-            reader = emcee.backends.HDFBackend(output_name+"-{}.h5".format(index), read_only=True)
-        except:
-            print("Max index was {} (no h5)".format(index-1))
-            break
+    for index in range(len(kernel)):
+        reader = emcee.backends.HDFBackend(output_name+"-{}.h5".format(index), read_only=True)
 
         try:
             tau = reader.get_autocorr_time()
@@ -86,25 +85,19 @@ def process(output_name):
             thin = 1
             burnin = 1000
 
-        try:
-            samples = reader.get_chain(discard=burnin, thin=thin)
-        except:
-            print("Max index was {} (broken h5)".format(index-1))
-            break
+        samples = reader.get_chain(discard=burnin, thin=thin)
         #log_prob_samples = reader.get_log_prob(discard=burnin, thin=thin)
         #log_prior_samples = reader.get_blobs(discard=burnin, thin=thin)
 
+        flat_samples = np.array([samples[:,:,i].flatten() for i in range(len(theta_true))])
 
-        flat_samples = np.array([samples[:,:,i].flatten() for i in range(3)])
         np.savetxt(output_name+"-{}-samples.dat".format(index), flat_samples)
-        index += 1
 
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        output_name = sys.argv[1]
+        terminal(output_name)
 
-if len(sys.argv) not in [2, 3]:
-    for item in os.listdir("../../staged/"):
-        try:
-            process(item[:-4])
-        except:
-            print("{} failed".format(item))
-else:
-    process(sys.argv[1])
+    else:
+        for name in os.listdir('../../staged'):
+            terminal(name)
