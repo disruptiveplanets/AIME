fa939416a1c228353f80a8d0d7d418bcc30e1f33
jack-dinsmore <goodsirmango@gmail.com>
2022-01-16 19:01:09 -0500

    Staged oblateness

--- data/oblateness/readme.md
+++ data/oblateness/readme.md
@@ -1,3 +1,7 @@
 ## Nov 14
 
 I started this oblateness scan. Scanning from 1e-2 to 1e-4, which covers most planets except Mercury and Venus.
+
+## Jan 16
+
+After solving the minimization bugs, I really started the oblateness scan. THe high sigma scan is still going.
--- data/oblateness/scatter.py
+++ data/oblateness/scatter.py
@@ -2,13 +2,13 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-BASE_NAME = "ob-0"
+BASE_NAME = "ob"
 SPECIALS = [(0.39269908169, 0, -0.09766608), (0.39269908169, 0.05200629, -0.2021978)]
 HIGH_ORDER = [0, 0, 0, 0, 0, 0, 0]
+NUM_GENERATE = 24
 
 def get_text(i, j20):
     return """2, 2
-3, 1
 120
 5
 1000
@@ -18,11 +18,12 @@ def get_text(i, j20):
 {}, {}, {}
 0.78539816339, 0.12499, -0.0001
 -0.78539816339, -0.12499, -0.24999
-0.01""".format(j20, SPECIALS[i][0], SPECIALS[i][1], SPECIALS[i][2])
+0.01, 1e-5""".format(j20, SPECIALS[i][0], SPECIALS[i][1], SPECIALS[i][2])
 
 for i in range(len(SPECIALS)):
-    for j2_index, j2 in enumerate(10**np.linspace(-4, -2, 20)):
+    if i == 0: continue
+    for j2_index, oblateness in enumerate(10**np.linspace(-4, -2, NUM_GENERATE)):
         name ="../../staged/{}-{}-{:02}.txt".format(BASE_NAME, i, j2_index)
         f = open(name, 'w')
-        f.write(get_text(i, -j2 * 2))
+        f.write(get_text(i, -oblateness * 2))
         f.close()
