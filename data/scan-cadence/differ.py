9acb058df5d4cc85aebe2b6504143bf33ddc0d1d
jack-dinsmore <goodsirmango@gmail.com>
2022-01-26 21:55:06 -0500

    Data from cadence

--- data/scan-cadence/differ.py
+++ data/scan-cadence/differ.py
@@ -9,8 +9,9 @@ param_names = ["\\gamma_0", "K_{22}", "K_{20}", "\Re K_{33}", "\Im K_{33}", "\Re
 
 percentiles = {}
 name_index = {}
-sigma_theta = None
+true_sigma = []
 cadences = []
+THETA_TRUE = []
 
 AXIS_SIZE = 12
 LEGEND_SIZE = 12
@@ -18,7 +19,11 @@ LEGEND_SIZE = 12
 N_DIM = None
 N_PERCENTILES = None
 
-SCALE_Y = 1.1
+def plot_best_fit(ax, xs, ys, scale):
+    slope = np.cov(ys, xs)[0][1] / np.var(xs    )
+    yint = np.mean(ys) - slope * np.mean(xs)
+    ax.plot(xs, (xs * slope + yint) * scale, color='k', linewidth=1, linestyle='dotted')
+    return slope
 
 # Get percentiles
 with open("percentiles.dat", 'r') as f:
@@ -35,7 +40,6 @@ with open("percentiles.dat", 'r') as f:
         percentiles[name] = perc_array
 
 # Get true sigmas
-index = 0
 for name in percentiles.keys():
     dir_name = name[:6]
     with open(f"{dir_name}/{dir_name}.txt", 'r') as f:
@@ -51,12 +55,10 @@ for name in percentiles.keys():
         theta_high = [float(x) for x in f.readline().split(',')]
         theta_low = [float(x) for x in f.readline().split(',')]
         sigma = [float(d) for d in f.readline().split(',')]
-    name_index[name] = index
-    index += 1
-    sigma_theta = sigma[0]
+    THETA_TRUE = theta_true
+    name_index[name] = int(dir_name[-2:])
+    true_sigma = sigma[0]
     cadences.append(cadence / 60)
-
-cadences = np.array(cadences)
 
 fig, axs = plt.subplots(figsize=(14, 8), ncols=3, nrows=4, sharex=True)
 axs = axs.reshape(-1)
@@ -68,35 +70,35 @@ for plot_index in range(N_DIM + 1):
     param_data = np.zeros(len(cadences) * N_PERCENTILES).reshape(N_PERCENTILES, len(cadences))
     for f in percentiles.keys():
         param_data[:,name_index[f]] = percentiles[f][i]
-    scale = 1 if i >= 3 else 1e4
+    scale = 1e4 if i < 3 else 1
+    axs[plot_index].plot(cadences, (param_data[1] - THETA_TRUE[i]) * scale / true_sigma, color=f"C{i}", linewidth=1)
+    axs[plot_index].plot(cadences, (param_data[-1] - THETA_TRUE[i]) * scale / true_sigma, color=f"C{i}", linewidth=1)
+    axs[plot_index].fill_between(cadences, (param_data[1]-THETA_TRUE[i]) * scale / true_sigma, 
+        (param_data[-1]-THETA_TRUE[i]) * scale / true_sigma,  color=f"C{i}", alpha=0.3)
 
-    axs[plot_index].plot(cadences, (param_data[1]-param_data[0]) / sigma_theta * scale, color=f"C{i}", linewidth=1)
-    axs[plot_index].plot(cadences, (param_data[-1]-param_data[0]) / sigma_theta * scale, color=f"C{i}", linewidth=1)
-    axs[plot_index].fill_between(cadences, (param_data[1]-param_data[0]) / sigma_theta * scale, 
-        (param_data[-1]-param_data[0]) / sigma_theta * scale,  color=f"C{i}", alpha=0.3)
+    axs[plot_index].plot(cadences, (param_data[2]-THETA_TRUE[i]) * scale / true_sigma, color=f"C{i}", linewidth=1)
+    axs[plot_index].plot(cadences, (param_data[-2]-THETA_TRUE[i]) * scale / true_sigma, color=f"C{i}", linewidth=1)
+    axs[plot_index].fill_between(cadences, (param_data[2]-THETA_TRUE[i]) * scale / true_sigma,
+        (param_data[-2]-THETA_TRUE[i]) * scale / true_sigma, color=f"C{i}", alpha=0.3)
 
-    axs[plot_index].plot(cadences, (param_data[2]-param_data[0]) / sigma_theta * scale, color=f"C{i}", linewidth=1)
-    axs[plot_index].plot(cadences, (param_data[-2]-param_data[0]) / sigma_theta * scale, color=f"C{i}", linewidth=1)
-    axs[plot_index].fill_between(cadences, (param_data[2]-param_data[0]) / sigma_theta * scale,
-        (param_data[-2]-param_data[0]) / sigma_theta * scale, color=f"C{i}", alpha=0.3)
-
-    axs[plot_index].plot(cadences, (param_data[3]-param_data[0]) / sigma_theta * scale, color=f"C{i}", linewidth=1, linestyle='dashed')
-
-    y_min_norm = np.min((param_data[-1]-param_data[0]) / sigma_theta * scale)
-    y_max_norm = np.max((param_data[1]-param_data[0]) / sigma_theta * scale)
-    axs[plot_index].set_ylim(y_min_norm * SCALE_Y, y_max_norm * SCALE_Y)
-
-    if i >= 3:
-        axs[plot_index].set_ylabel(f"$\sigma({param_names[i]}) / \sigma_\\theta$", size=AXIS_SIZE)
-    else:
-        axs[plot_index].set_ylabel(f"$\sigma({param_names[i]}) / \sigma_\\theta$ ($\\times 10^{{-4}}$)", size=AXIS_SIZE)
+    axs[plot_index].plot(cadences, (param_data[3]-THETA_TRUE[i]) * scale / true_sigma, color=f"C{i}", linewidth=1, linestyle='dashed')
+    axs[plot_index].axhline(y=0, color="k", linewidth=1)
 
     #axs[i].set_xscale('log')
     #axs[i].set_yscale('log')
+    if i >= 3:
+        axs[plot_index].set_ylabel(f"$|\Delta {param_names[i]}| / \sigma_\\theta$", size=AXIS_SIZE)
+    else:
+        axs[plot_index].set_ylabel(f"$|\Delta {param_names[i]}| / \sigma_\\theta\ (\\times 10^{{-4}})$", size=AXIS_SIZE)
 
     if plot_index in [6, 8, 10]:
         axs[plot_index].set_xlabel(f"$\Delta t$ (min)")
 
+    EXTENT_AMOUNT = 100
+    low = (param_data[-2]-param_data[0])*EXTENT_AMOUNT + param_data[0] - THETA_TRUE[i]
+    high = (param_data[2]-param_data[0])*EXTENT_AMOUNT + param_data[0] - THETA_TRUE[i]
+    mid = param_data[0] - THETA_TRUE[i]
+    trues = ((low < 0) & (0 < mid)) | (mid < 0) & (0 < low) | ((high < 0) & (0 < mid)) | (mid < 0) & (0 < high)
     i += 1
 
 axs[9].remove()
@@ -105,12 +107,13 @@ axs[11].remove()
 custom_lines = [Line2D([0], [0], color='k', lw=4, alpha=0.3),
                 Line2D([0], [0], color='k', lw=4, alpha=0.6),
                 Line2D([0], [0], color='k', lw=1, linestyle='dashed')]
-fig.legend(custom_lines, ['95\%', '68\%', '50\%'], ncol=3, loc='lower right', prop={'size': LEGEND_SIZE})
+fig.legend(custom_lines, ['95\%', '68\%', '50\%'], ncol=3, framealpha=1, loc="lower right", prop={'size': LEGEND_SIZE})
 fig.tight_layout()
+
 
 line = plt.Line2D([0,1],[0.77, 0.77], transform=fig.transFigure, color="black")
 fig.add_artist(line)
 
-plt.savefig("cadences.pdf")
-plt.savefig("cadences.png")
+plt.savefig("differ.pdf")
+plt.savefig("differ.png")
 plt.show()
--- data/scan-cadence/readme.md
+++ data/scan-cadence/readme.md
@@ -2,7 +2,11 @@
 
 Pulled part of fit (all except 00) today. There are a few things to check up on:
 
-1. Why does redchi decrease for higher cadence?
-2. Demonstrate closeness of Klms fitted to true
-3. Make the plot separate the tiers like the sigma plots
-4. Pull last cadence
+1. Why does redchi decrease for higher cadence? (It doesn't; there's just more scatter for high cadence because there's less data)
+2. Demonstrate closeness of Klms fitted to true (Done)
+3. Make the plot separate the tiers like the sigma plots (Done)
+4. Pull last cadence (Done)
+
+# Jan 26 (After wisdom teeth hiatus)
+
+Solved above problems, including a klm true to fit comparison. I also put this in the paper. The only thing left to do is to figure out this critical time that sets the lump in cadence.
