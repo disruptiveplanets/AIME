Fixed exotic moment calculation

code/density/core.py
@@ -163,7 +163,7 @@ class TrueShape:
 
     def smooth_y(am, k22, k20, com):
         b = np.sqrt(5/3) * am * np.sqrt(1 - 2 * k20 - 12 * k22)
-        g = 20 / 3 * com / b**2
+        g = 5 * com / b**2
         return lambda x, y, z: 1 + g * y
 
     def blob(am, k22, k20):

code/other-figs/delta-chisq.py
@@ -2,23 +2,78 @@ import numpy as np
 
 TRUE_CENTER_OF_MASS = [0, 15.839935068775791, 0]
 
-TRUE_MOMENTS = [
-    -0.05200629, -0.2021978,
-    0, 0,
-    0, 0,
-    0, 0,
-    0
-]
+TRUE_MOMENTS = {
+    "sph-3": [
+        
+    ],
+    "move-1.5": [
+        -0.05195945615840046, -0.20085472167788565,
+        0, -0.0003845813086399978,
+        0, 0,
+        0, -0.0019368958923091067,
+    ],
+} # Uncorrected
 
-UNIFORM_MOMENTS = [
-    -0.05195945615840046, -0.20085472167788565,
-    0, -0.0003845813086399978,
-    0, 0,
-    0, -0.0019368958923091067,
-] # Uncorrected
-ELLIPSOID_AM = 1000
-BULK_AM = 1000
+UNIFORM_MOMENTS = {
+    "sph-3": [
+        -0.05200629, -0.2021978,
+        0, 0,
+        0, 0,
+        0, 0,
+        0
+    ],
+    "move-1.5": [
+       
+    ], # Smoothed
+} # Uncorrected
 
-samples = np.load("../density/samples/den-cpre-move-1.5-0-samples.npy")
+SAMPLE_NAMES = {
+    "sph-3": "../density/samples/den-core-sph-3-0-samples.npy",
+    "move-1.5": "../density/samples/den-core-move-1.5-0-samples.npy",
+}
+BULK_AMS = {
+    "sph-3",
+    "move-1.5": ,
+}
+SURFACE_AMS = {
+    "sph-3": 1000,
+    "move-1.5": ,
+}
+N_MOMENTS = 9
 
-## Compute chi squared agreement between true moments and data and between uniform moments and data and compute the delta chisquared 
+
+def correct_moments(moments, bulk, surf):
+    ls = [2, 2, 3, 3, 3, 3, 3, 3, 3]
+    results = []
+    for moment, l in zip(moments, ls):
+        results.append(moment * (bulk / surf)** (l - 2))
+    return results
+
+def chisq(means, true, cov):
+    pass
+
+def calc_dsq(name):
+    print(name)
+    samples = np.load("../density/samples/den-core-move-1.5-0-samples.npy").reshape(-1, 10)[:, 1:]
+    print(samples.shape)
+
+    corrected_samples = np.array([correct_moments(s) for s in samples])
+
+    means = np.mean(corrected_samples, axis=0)
+    print(means)
+    cov = np.cov()
+    print(cov.shape)
+
+    true_unif = correct_moments(UNIFORM_MOMENTS[name])
+    true_true = correct_moments(TRUE_MOMENTS[name])
+
+    unif_chisq = chisq(means, true_unif, cov)
+    true_chisq = chisq(means, true_true, cov)
+
+    print(f"True chisq\t {true_chisq} ({true_chisq / N_MOMENTS})")
+    print(f"Unif chisq\t {unif_chisq} ({unif_chisq / N_MOMENTS})")
+    print(f"Delta chisq\t {unif_chisq - true_chisq} ({(unif_chisq - true_chisq) / N_MOMENTS})")
+
+if __name__ == "__main__":
+    calc_dsq("move-1.5")
+    calc_dsq("sph-3")